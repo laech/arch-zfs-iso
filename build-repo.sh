7215e0ed5489c49b53201aac0a0cfd37f6a0130f
Lae <laec@protonmail.ch>
2017-07-01 18:19:57 +1200

    Update to ZFS 0.6.5.10.

--- build-repo.sh
+++ build-repo.sh
@@ -25,13 +25,13 @@ build_zfs() {
     rm -rf "${tmpdir}"
     mkdir "${tmpdir}"
 
-    # Version 0.6.5.9-1
-    wget -nv -O "${tmpdir}/zfs-dkms.tar.gz" "${baseurl}/aur-8c86abf3c155bcbf6489d43c9c2a91ae463dc025.tar.gz"
-    wget -nv -O "${tmpdir}/spl-dkms.tar.gz" "${baseurl}/aur-42b5f8b22efc842cba61bd968a3a7fd722de9a45.tar.gz"
+    # Version 0.6.5.10
+    wget -nv -O "${tmpdir}/zfs-dkms.tar.gz" "${baseurl}/aur-3833b029fc38d499e2b5a7b2c25c2280134ad0f0.tar.gz"
+    wget -nv -O "${tmpdir}/spl-dkms.tar.gz" "${baseurl}/aur-74c5db638b2f072e54ff1caef451140bbb94ddc6.tar.gz"
 
     sha512sum -c <<EOF
-7e6873be2c9e98cb27476548245c6bb81477d072d96c54cddb399582f70d828386d7a6ef4b46d1bf4054e25890e2ee4b060cdc42e524810e626b7f8490eece95  ${tmpdir}/spl-dkms.tar.gz
-3c2003a3fb0897102a8303d1bc6b2a0e8a7d22cd74281403a5ef715233090a5d8f2e2a692f6497c93e65b58125e359df5c36ece1ee7a44a3cba2160003b7a8fc  ${tmpdir}/zfs-dkms.tar.gz
+7cf86d8281e7862c5125c19f59faf09bc7f43398973a123e6abea57d987b7849521775b2de9ca47c2aafeec793c9c23242bcfec6aef4fe4e20b32f261933e7df  ${tmpdir}/spl-dkms.tar.gz
+9e6a3170b6821707657b7a6fc4510c1243ea907c6dfe23485e6c68b98e8195b28e117ba4878ee4bddfe53c143c68f2c8199c9673ed2b2d1acfaa5b183718bad6  ${tmpdir}/zfs-dkms.tar.gz
 EOF
 
     mkdir "${tmpdir}/spl-dkms" && tar -xzvf "${tmpdir}/spl-dkms.tar.gz" -C "${tmpdir}/spl-dkms" --strip-component 1
