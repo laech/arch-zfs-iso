d3b0d494c69541a414ebc5095017b8ac96c2bc39
Lae <llaechen@gmail.com>
2019-05-28 22:03:33 +1200

    Update to ZFS 0.8.0 and switch to archzfs repo.

--- build-archiso.sh
+++ build-archiso.sh
@@ -1,18 +1,19 @@
 #!/bin/env bash
-#
-# Builds a Arch Linux live image with ZFS repo created by build-repo.sh
-#
 
 set -o errexit
 set -o nounset
 set -o pipefail
-set -o xtrace
 
 readonly workdir="$(dirname $0)"
 readonly archdir="${workdir}/archlive"
 
 pacman -Qs archiso > /dev/null || {
-    echo "error: archiso is not installed"
+    echo "archiso is not installed, installing..."
+    sudo pacman --noconfirm -S archiso
+}
+
+pacman -Qs archiso > /dev/null || {
+    echo "archiso is not installed."
     exit 1
 }
 
@@ -22,37 +23,22 @@ mkdir "${archdir}"
 cp -r /usr/share/archiso/configs/releng/* "${archdir}"
 
 cat <<EOF >> "${archdir}"/pacman.conf
-[zfs]
-SigLevel = Optional TrustAll
-Server = file://$(realpath "${workdir}")/repo
+[archzfs]
+Server = http://archzfs.com/\$repo/x86_64
 EOF
 
-#
-# Install zfs-dkms, need to explicitly specify linux-headers as it not
-# automatically pulled in.
-#
 cat <<EOF >> "${archdir}"/packages.x86_64
 linux-headers
-zfs-dkms
+archzfs-dkms
 EOF
 
-#
-# When DKMS tries to install zfs-dkms before spl-dkms, zfs-dkms will error
-# with (but doesn't stop the process):
-#
-#   configure: error:
-#           *** Please make sure the kmod spl devel <kernel> package for your
-#           *** distribution is installed then try again.  If that fails you
-#           *** can specify the location of the spl objects with the
-#           *** '--with-spl-obj=PATH' option.
-#
-# since zfs-dkms depends on spl-dkms. Doing `dkms autoinstall` after DKMS has
-# gone through everything include spl-dkms, will get zfs-dkms installed.
-#
 cat <<EOF >> "${archdir}"/airootfs/root/customize_airootfs.sh
-dkms autoinstall
 echo zfs > /etc/modules-load.d/zfs.conf
 EOF
+
+# archzfs key
+sudo pacman-key --recv-keys F75D9D76
+sudo pacman-key --lsign-key F75D9D76
 
 rm -rf "${archdir}/out"
 mkdir "${archdir}/out"
