2de612d4ee9efbcbb272fa342842889c3527a8bb
Lae <llaechen@gmail.com>
2019-06-04 23:02:06 +1200

    Option to build dkms or normal ZFS package. Sign ZFS repo keys. Update tests.

--- build-archiso.sh
+++ build-archiso.sh
@@ -3,6 +3,9 @@
 set -o errexit
 set -o nounset
 set -o pipefail
+set -o xtrace
+
+readonly use_dkms=${use_dkms:-0}
 
 readonly workdir="$(dirname $0)"
 readonly archdir="${workdir}/archlive"
@@ -27,18 +30,33 @@ cat <<EOF >> "${archdir}"/pacman.conf
 Server = http://archzfs.com/\$repo/x86_64
 EOF
 
-cat <<EOF >> "${archdir}"/packages.x86_64
-linux-headers
-archzfs-dkms
-EOF
+if [[ $use_dkms == 1 ]]; then
 
-cat <<EOF >> "${archdir}"/airootfs/root/customize_airootfs.sh
-echo zfs > /etc/modules-load.d/zfs.conf
-EOF
+    echo "linux-headers archzfs-dkms" \
+         >> "${archdir}"/packages.x86_64
+
+    echo 'echo zfs > /etc/modules-load.d/zfs.conf' \
+         >> "${archdir}"/airootfs/root/customize_airootfs.sh
+else
+    echo "archzfs-linux" >> "${archdir}"/packages.x86_64
+fi
 
 # archzfs key
 sudo pacman-key --recv-keys F75D9D76
 sudo pacman-key --lsign-key F75D9D76
+
+readonly archzfs_gpg=$(pacman-key --export F75D9D76)
+
+cat <<EOF >> "${archdir}"/airootfs/root/customize_airootfs.sh
+
+echo '$archzfs_gpg' > /usr/share/pacman/keyrings/archzfs.gpg
+
+sed -i 's|\[Install\]|ExecStart=/usr/bin/pacman-key --populate archzfs\n\[Install\]|' \
+    /etc/systemd/system/pacman-init.service
+
+sed -i 's|\[Install\]|ExecStart=/usr/bin/pacman-key --lsign-key F75D9D76\n\[Install\]|' \
+    /etc/systemd/system/pacman-init.service
+EOF
 
 rm -rf "${archdir}/out"
 mkdir "${archdir}/out"
